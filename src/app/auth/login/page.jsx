b1ff53734318d2275479b0cc6d012d20a3f60874
AlvaroMartinNarganes <alvaro_martin_narganes02@hotmail.com>
2023-12-01 14:21:58 +0100

    Creación del proyecto, inicio del auth

---
+++ src/app/auth/login/page.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Login = () => {
+    return (
+        <div>
+          <h1 className='text-3xl font-bold'>HOLA MUNDO DESDE LOGIN</h1>
+        </div>
+    );
+};
+export default Login;
---
+++ src/app/page.jsx
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+
+export default function Home() {
+    return (
+        <>
+            <main className="flex min-h-screen flex-col items-center justify-between p-24">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet assumenda, beatae dicta earum eius
+                    esse eveniet
+                    expedita facere illum incidunt maxime minima neque nihil nobis possimus similique sit ut veniam.</p>
+            </main>
+        </>
+    )
+}
