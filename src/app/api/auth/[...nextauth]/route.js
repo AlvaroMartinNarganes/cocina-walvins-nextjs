ca20b3287e3a890689246481a22c97b95d2220ad
AlvaroMartinNarganes <alvaro_martin_narganes02@hotmail.com>
2023-12-11 14:04:53 +0100

    Completado navbar, inicio de sesion ya emite el identificador del usuario

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,13 +1,44 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import Credentials from "next-auth/providers/credentials";
 
 //const handler=NextAuth(options);
-const handler =NextAuth({
-    providers:[
+const handler = NextAuth({
+    secret: process.env.AUTH_SECRET,
+    providers: [
+        Credentials({
+            name: "user",
+            credentials: {
+                username: {label: 'User', type: 'text'},
+                password: {label: 'Contraseña', type: 'password'}
+            },
+            async authorize(credentials, req) {
+                //TODO: En un futuro conectar con la base de datos, aquí vamos a probar solo que funciona bien
+                const user = {id: '1', name: 'walvins', email: 'walvinstheoriginal@gmail.com', pass: '123456'}
+                //Comprobar que el usuario existe en la bd
+                if (credentials.username === user.name && credentials.password === user.pass) {
+                    console.log('ta chevere dale paso')
+                    return user
+                }
+                return null
+            }
+        }),
         GoogleProvider({
             clientId: process.env.GOOGLE_ID,
-            clientSecret:process.env.GOOGLE_SECRET,
+            clientSecret: process.env.GOOGLE_SECRET,
         })
-    ]
+    ],
+    callbacks: {
+        //JWT devuelve un valor token.sub que sirve como identificador del usurio, usando este sub podremos
+        async jwt({token}) {
+            // Persist the OAuth access_token and or the user id to the token right after signin
+            return token
+        },
+        async session({session, token, user}) {
+            // Send properties to the client.
+            session.user.id = token.sub //Mandar la session con el identificador al cliente para poder hacer lo que necesite
+            return session
+        }
+    }
 })
 export {handler as GET, handler as POST}
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,22 +2,36 @@
 import React from 'react';
 import Link from "next/link";
 import {getServerSession} from "next-auth";
-import {useSession} from "next-auth/react";
+import {signIn, signOut, useSession} from "next-auth/react";
+import Image from "next/image";
 
 const Navbar = () => {
-    const {data:session}=useSession()
-    console.log(session)
+    const {data: session} = useSession()
+    const imageAvatarSyle={
+        borderRadius:'50%'
+    }
     return (
         <header className='bg-blue-300 '>
             <nav className='flex items-center justify-between w-full px-10 py-4'>
                 <div>
                     <h1 className='text-3xl font-bold'><Link href='/'>Cocina con Walvins</Link></h1>
                 </div>
-                <div className='flex gap-3'>
+                <div className=''>
                     {/*<Link href='/auth/register' className='hover:bg-blue-400 py-2 px-1'>Crear Cuenta</Link>*/}
-                    {session ? <Link href="/api/auth/signout?callbackUrl=/" className='hover:bg-blue-400 py-2 px-1'>Cerrar
-                            sesión</Link> :
-                        <Link href='api/auth/signin?callbackUrl=/' className='hover:bg-blue-400 py-2 px-1'>Iniciar Sesión</Link>}
+                    {session ? <div className='flex flex-row items-center uppercase gap-2'>
+                            {session.user.image ?
+                                <Image src={session.user.image} width={40}
+                                       height={40}
+                                       alt={`Foto de perfil de ${session.user.name}`}
+                                       style={imageAvatarSyle}
+                                /> : <></>}
+                            <p className='font-bold'>{session.user.name}</p>
+                            <button onClick={() => signOut()} className='hover:bg-blue-400 rounded-xl py-2 px-1'>Cerrar
+                                sesión
+                            </button>
+                        </div> :
+                        <button onClick={() => signIn()} className='hover:bg-blue-400 rounded-xl py-2 px-1'>Iniciar
+                            Sesión</button>}
                 </div>
             </nav>
         </header>
